f7bb64405df00c7eeb5d56fc9463a289d331c6bf
RaaaM <aymanmu157@gmail.com>
2026-07-16 19:53:29 +0300

    test cookie

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -4,37 +4,23 @@ export async function POST(request: Request) {
 
   const { password } = await request.json();
 
-
   if (password !== "1/3/2006") {
-
     return NextResponse.json(
-      {
-        error: "Wrong password"
-      },
-      {
-        status: 401
-      }
+      { error: "Wrong password" },
+      { status: 401 }
     );
-
   }
-
 
   const response = NextResponse.json({
     success: true,
   });
 
-
-  response.cookies.set({
-    name: "admin",
-    value: "true",
+  response.cookies.set("admin", "true", {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    sameSite: "lax",
     path: "/",
     maxAge: 60 * 60 * 24 * 30,
   });
 
-
   return response;
-
 }
