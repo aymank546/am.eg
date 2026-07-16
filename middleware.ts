3c2428676f33faff30dddf78fe65bc6afd7a733c
RaaaM <aymanmu157@gmail.com>
2026-07-16 19:33:58 +0300

    debug admin cookie

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,25 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-
 export function middleware(request: NextRequest) {
 
   const admin = request.cookies.get("admin")?.value;
 
+  console.log("ADMIN COOKIE:", admin);
+
 
   if (
-    request.nextUrl.pathname.startsWith("/dashboard")
-    &&
+    request.nextUrl.pathname.startsWith("/dashboard") &&
     admin !== "true"
   ) {
-
     return NextResponse.redirect(
       new URL("/login", request.url)
     );
-
   }
 
 
   return NextResponse.next();
-
 }
-
 
 
 export const config = {
