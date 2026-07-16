8fba471f42a968d59af516ce7ce68f571bd6c9be
RaaaM <aymanmu157@gmail.com>
2026-07-16 20:07:55 +0300

    remove middleware

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,36 +2,29 @@
 
 import { useState } from "react";
 
+export default function LoginPage() {
 
-export default function LoginPage(){
+  const [password, setPassword] = useState("");
 
-  const [password,setPassword] = useState("");
+  async function login() {
 
-
-
-  async function login(){
-
-    const res = await fetch("/api/login",{
-
-      method:"POST",
-
-      headers:{
-        "Content-Type":"application/json"
+    const res = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-
-      body:JSON.stringify({
-        password: password
-      })
-
+      body: JSON.stringify({
+        password,
+      }),
     });
 
+    if (res.ok) {
 
+      localStorage.setItem("admin", "true");
 
-    if(res.ok){
+      window.location.href = "/dashboard";
 
-      window.location.href="/dashboard";
-
-    }else{
+    } else {
 
       alert("كلمة السر غلط");
 
@@ -39,51 +32,32 @@ export default function LoginPage(){
 
   }
 
-
-
   return (
 
     <main className="flex min-h-screen items-center justify-center bg-gray-100">
 
-
       <div className="rounded-xl bg-white p-8 shadow">
-
 
         <h1 className="mb-5 text-3xl font-bold">
           Admin Login
         </h1>
 
-
         <input
-
           type="password"
-
           className="mb-4 border p-3"
-
           placeholder="Password"
-
           value={password}
-
-          onChange={(e)=>setPassword(e.target.value)}
-
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-
         <button
-
           onClick={login}
-
           className="bg-black px-6 py-3 text-white"
-
         >
-
           دخول
-
         </button>
 
-
       </div>
-
 
     </main>
 
